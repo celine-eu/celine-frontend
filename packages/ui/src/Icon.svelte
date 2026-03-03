e8f1e42da9b8e215f3c45747b7eeae751f1c19a3
lc-spxl <luca.capra@spindox.it>
2026-03-03 08:13:07 +0100

    fix: add users icon

--- packages/ui/src/Icon.svelte
+++ packages/ui/src/Icon.svelte
@@ -54,6 +54,7 @@
     | "paperclip"
     | "image"
     | "log-out"
+    | "users"
     // Chat
     | "message-circle"
     | "message-square"
@@ -139,6 +140,8 @@
       "M19 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2z M8.5 10a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z M21 15l-5-5L5 21",
     "log-out":
       "M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4 M16 17l5-5-5-5 M21 12H9",
+    users:
+      "M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2 M9 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8z M23 21v-2a4 4 0 0 0-3-3.87 M16 3.13a4 4 0 0 1 0 7.75",
 
     // Chat
     "message-circle":
